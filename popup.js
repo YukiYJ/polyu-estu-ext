5f198a728a7a9ac68ea64ce84d29d61b0d72570c
derekmma <hi@derek.ma>
2017-09-12 00:05:06 +0800

    structure

--- popup.js
+++ popup.js
@@ -2,15 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 function click(e) {
-  chrome.tabs.executeScript(null,
-    //   {code:"document.body.style.backgroundColor='" + e.target.id + "'"}
+//   chrome.tabs.executeScript(null,
+    // //   {code:"document.body.style.backgroundColor='" + e.target.id + "'"}
     //   {code:"document.getElementById('login-button').click();"});
-    {code:"var box1 = document.getElementById('mainForm:yearSemDropDown'); \
-            box1.value = 1495;\
-            document.getElementById('mainForm:nextButton').click();\
-      "});
+    // // {code:" window.location.href = 'https://www38.polyu.edu.hk/eStudent/secure/my-subject-registration/subject-register-select-acad-year-sem.jsf';\
+    // //         var box1 = document.getElementById('mainForm:yearSemDropDown'); \
+    // //         box1.value = 1495;\
+    // //         document.getElementById('mainForm:nextButton').click();\
+    // //   "});
+
+    // chrome.tabs.executeScript(null,
+    //       {code:"window.location.href = 'https://www38.polyu.edu.hk/eStudent/secure/my-subject-registration/subject-register-select-acad-year-sem.jsf';"});
+    
+    chrome.tabs.executeScript({
+        file: 'simulate.js'
+    });  
   window.close();
 }
 
---
+++ simulate.js
@@ -0,0 +1,2 @@
+document.getElementById('login-button').click();
+window.location.href = 'https://www38.polyu.edu.hk/eStudent/secure/my-subject-registration/subject-register-select-acad-year-sem.jsf';
